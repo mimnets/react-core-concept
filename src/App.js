4cb4bd06edae5d064d8cdaa5053435091918f94d
MiMNets <mdmonirulislammonir@gmail.com>
2020-08-21 01:34:18 +0600

    product as object

--- src/App.js
+++ src/App.js
@@ -27,14 +27,56 @@ function App() {
   <h3 style={{color: 'gray',
     backgroundColor: 'cyan'}}>Singer: {person2.name + " " + person2.job}</h3>
         <p>My first react paragraph</p>
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Person name={nayoks[0]} nayika={nayikas[4]}></Person>
+        <Person name="Jashim" nayika="Shabana"></Person>
         <Person></Person>
       </header>
     </div>
   );
 }
 
-function Person(){
-  return <h1>Name: Sakib</h1>
+const products = [
+  {name: 'Photoshop', price:'$90.99'},
+  {name: 'Illustrator', price:'$99.99'},
+  {name: 'Adobe Acrobat', price:'$06.99'}
+]
+
+
+function Product(props){
+  const productStyle = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: '200px',
+    width: '200px',
+    float: 'left'
+  }
+  const {name, price} = props.product;
+  return (
+    <div style={productStyle}>
+      <h3>{name}</h3>
+      <h5>{price}</h5>
+      <button>Buy now</button>
+    </div>
+  )
+}
+
+const nayoks = ['Anowar Khan', 'Jafor Iqbal', 'Alomgir', 'Salman Shah']
+const nayikas = ['Moushumi', 'Shabana', 'Bobita', 'Diti', 'Anowara Begum']
+function Person(props){
+  const personStyle={
+    border: '2px solid red', 
+    margin: '10px'
+  }
+  return (
+  <div style={personStyle}>
+  <h1>Nayok: {props.name}</h1>
+  <h1>Nayika: {props.nayika}</h1>
+  <h3>Hero of the year</h3>
+  </div>
+  )
 }
 
 export default App;
