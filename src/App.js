727bf071948aaac9aebdc34069a1f1d94778fe45
MiMNets <mdmonirulislammonir@gmail.com>
2020-08-21 21:32:17 +0600

    call users and set to ul as list

--- src/App.js
+++ src/App.js
@@ -76,11 +76,16 @@ function Users(){
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.json())
-    .then( data => console.log(data));
-  })
+    .then( data => setUsers(data));
+  }, [])
   return(
     <div>
-      <h3>Dynamic Users</h3>
+      <h3>Dynamic Users : {users.length}</h3>
+      <ul>
+        {
+          users.map(user => <li>{user.name}</li>)
+        }
+      </ul>
     </div>
   )
 }
